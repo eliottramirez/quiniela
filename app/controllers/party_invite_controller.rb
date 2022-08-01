ed664a1a308e9ded28176548d759149f02b29578
Eliott Ramírez <eliottrg@gmail.com>
2022-08-01 14:10:08 -0500

    Fix bug.

--- app/controllers/party_invite_controller.rb
+++ app/controllers/party_invite_controller.rb
@@ -4,13 +4,13 @@ class PartyInviteController < ApplicationController
       PartyInviteJob.perform_later(invite_params)
       redirect_to party_path(params[:party]), notice: "Invitación enviada."
     else
-      redirect_to party_path(params[:party]), alert: "El campo \"Email\" no puede ir en blanco."
+      redirect_to party_path(Party.find(params[:party])), alert: "El campo \"Email\" no puede ir en blanco."
     end
   end
 
   private
 
   def invite_params
-    params.permit(:from_user, :party, :party_name, :sharing_code, :to_email)
+    params.permit(:commit, :from_user, :party, :party_name, :sharing_code, :to_email)
   end
 end
