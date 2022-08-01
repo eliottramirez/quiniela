2bf66e247dd45ac098af99134b04b5dd53381981
Eliott Ramírez <eliottrg@gmail.com>
2022-07-31 23:40:00 -0500

    Add avatars to User seeds.

---
+++ db/seeds/003_users.rb
@@ -0,0 +1,22 @@
+User.create(name: "Eliott", email: "eliott@gmail.com", password: "123456", super_admin: true)
+
+[
+  "Valentina",
+  "Santiago",
+  "Ximena",
+  "Leonardo",
+  "Camila",
+  "Diego"
+].each do |name|
+  user = User.create(
+    name: name,
+    email: "#{name.downcase.gsub(' ', '_')}@mail.com",
+    password: "123456"
+  )
+
+  user.avatar.attach(
+    io: File.open("db/seeds/avatars/#{name.downcase.gsub(' ', '_')}.jpeg"),
+    filename: "00#{user.id}.jpeg",
+    content_type: "image/jpeg"
+  )
+end
